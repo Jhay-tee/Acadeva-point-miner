38efa812bf7b4167574aae705c9b0203323f30fd
Jhay-tee <akpanjonathan2006@gmail.com>
2025-07-31 17:56:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+let points = 0;
+let errorMessage = 'Please enter a number between 1 and 3'
+let numberToGuess = Math.floor(Math.random() * 3) + 1;
+const guessInput = document.getElementById('guess-input');
+const guessBtn = document.getElementById('guess-btn');
+const resultMessage = document.getElementById('result-message');
+const pointsBox = document.getElementById('points');
+const popup = document.getElementById('popup');
+const popupMessage = document.getElementById('popup-message');
+const popupClose = document.getElementById('popup-close');
+const welcome = document.getElementById("welcome");
+const welcomeMessage = document.getElementById("welcomeMessage");
+const ok = document.getElementById("ok")
+
+welcomeMessage.textContent = "Welcome to Acadeva Guess game get 50 points for any Correct guess enjoy!!";
+        welcome.style.display = 'block';
+ok.addEventListener('click', close);
+
+function close() {
+    welcome.style.display = 'none';
+}
+guessBtn.addEventListener('click', checkGuess);
+
+function checkGuess() {
+    const guess = parseInt(guessInput.value);
+    if (isNaN(guess) || guess < 1 || guess > 3) {
+        resultMessage.textContent = 'Please enter a number between 1 and 3';
+        resultMessage.style.color = "red";
+        //guessInput.value = " ";
+        setTimeout(() => {
+            resultMessage.textContent = '';
+            guessInput.value = " ";
+        }, 2000);
+        return;
+    }
+
+    if (guess === numberToGuess) {
+        points += 50;
+        pointsBox.textContent = `Current Points: ${points}`;
+        popupMessage.textContent = `Congratulations! You guessed the number correctly and earned 50 points!`;
+        popup.style.display = 'block';
+        numberToGuess = Math.floor(Math.random() * 3) + 1;
+        
+    } else {
+        resultMessage.textContent = `Incorrect guess the number was ${numberToGuess}`;
+        setTimeout(() => {
+            resultMessage.textContent = '';
+        }, 2000);
+        numberToGuess = Math.floor(Math.random() * 3) + 1;
+
+    }
+    guessInput.value = '';
+    
+}
+
+popupClose.addEventListener('click', closePopup);
+
+function closePopup() {
+    popup.style.display = 'none';}
